3c31892f91c5bd9f899c8e4da52822a358d99353
larako <yoola78@gmail.com>
2017-03-21 17:24:33 +0200

    Add files via upload

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -5,4 +5,8 @@ from django.template import loader
 def index(request):
     template = loader.get_template('polls/index.html')
     return render(request, 'polls/index.html',None)
+
+def article(request):
+    template = loader.get_template('polls/article.html')
+    return render(request, 'polls/article.html',None)
 # Create your views here.
