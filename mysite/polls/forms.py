1b8ec29c01000a7945a316c55230f293b8ea87d9
charlycmx <charlycmx@outlook.com>
2017-04-21 20:59:50 +0200

    connexion inscription deconnexion

--- mysite/polls/forms.py
+++ mysite/polls/forms.py
@@ -5,3 +5,13 @@ class SearchForm(forms.Form):
 	def search(self):
 
 		pass
+
+class ConnexionForm(forms.Form):
+    username = forms.CharField(label="Username", max_length=30)
+    password = forms.CharField(label="Password", widget=forms.PasswordInput)
+
+class SignupForm(forms.Form):
+    username = forms.CharField(label="Username", max_length=30)
+    password = forms.CharField(label="Password", widget=forms.PasswordInput)
+    confirm_password = forms.CharField(label="Confirm", widget=forms.PasswordInput)
+    email = forms.CharField(label="Email", widget=forms.EmailInput)
